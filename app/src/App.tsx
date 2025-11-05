6872dd3063699668a0bef349034dba095a2692e7
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2025-11-05 12:56:42 -0500

    clean up nav a bit

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { lazy } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import * as s from './App.module.css'
 
 const DetailView = lazy(() => import('./pages/DetailView'))
 const RegionalElectricityPatterns = lazy(() => import('./pages/ComparePlans'))
@@ -18,7 +19,7 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <nav>
+        <nav className={s.nav}>
           <Link to="/">Home</Link>
           <Link to="/detail">Detail View</Link>
           <Link to="/compare">Compare Plans</Link>
