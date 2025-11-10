cc352597abaa44e9c661a6fac92b93da02671154
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2025-11-10 15:00:48 -0500

    include wholesale data

---
+++ data/process-wholesale-data.py
@@ -0,0 +1,18 @@
+import polars as pl
+from pathlib import Path
+
+df = pl.read_excel(
+    [
+        Path("raw") / "wholesale" / f"ice_electric-202{year}final.xlsx"
+        for year in range(0, 5)
+    ]
+)
+df = df.rename({"Delivery \r\nend date": "Delivery end date"})
+
+import duckdb
+
+con = duckdb.connect("flattened.duckdb")
+
+# Or use DuckDB's register method for zero-copy
+con.register("raw_wholesale", df)
+con.execute("CREATE OR REPLACE TABLE wholesale AS SELECT * FROM raw_wholesale")
