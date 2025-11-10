5ff755873b88ca598c74270eb1430aa0113572ba
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2025-11-10 18:44:16 -0500

    put component at top

--- app/src/pages/DetailView.tsx
+++ app/src/pages/DetailView.tsx
@@ -1,18 +1,12 @@
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import { useImmer } from 'use-immer'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { RatePlanSelector } from '../components/RatePlanSelector'
 import { useRatePlanInData } from '../hooks/useRateInPlanData'
 import * as s from './DetailView.module.css'
 import { useRatePlan } from '../hooks/useRatePlan'
-import { DatePicker, Form } from 'antd'
+import { DatePicker, Form, Select } from 'antd'
 import { useVegaEmbed } from 'react-vega'
-
-interface State {
-  rawData?: string
-  adjustedIncluded: boolean
-  date: Dayjs
-}
 
 import type { TopLevelSpec } from 'vega-lite'
 import type {
@@ -24,6 +18,103 @@ import type {
 import dayjs, { Dayjs } from 'dayjs'
 import { useWholesaleData } from '../hooks/useWholesaleData'
 import { sum } from 'es-toolkit'
+import { HUB_DICT } from '../data/queries'
+
+interface State {
+  adjustedIncluded: boolean
+  date: Dayjs
+  wholesale: keyof typeof HUB_DICT
+}
+
+export default function DetailView() {
+  const { id: ratePlanParam } = useParams()
+  const { data: selectedPlan } = useRatePlan(ratePlanParam)
+
+  const { data: supersedesExistsInData } = useRatePlanInData(
+    selectedPlan?.supersedes
+  )
+  const [state, updateState] = useImmer<State>({
+    adjustedIncluded: true,
+    date: dayjs().set('year', 2024),
+    wholesale: 'New England',
+  })
+  const { data: wholesaleData } = useWholesaleData(state.wholesale, state.date)
+  const chartRef = useRef<HTMLDivElement>(null)
+  const retailData = pullData(selectedPlan, state.date.month())
+  const preparedWholesale = prepareWholesaleData(wholesaleData)
+  useVegaEmbed({
+    ref: chartRef,
+    spec: createPricingChartSpec(retailData, preparedWholesale),
+    options: { mode: 'vega-lite', actions: false },
+  })
+
+  const nav = useNavigate()
+
+  const handleRatePlanChange = async (value: string) => {
+    nav(`/detail/${value}`)
+  }
+
+  return (
+    <main className={s.main}>
+      <h1>Visualizing Dynamic Electricity Pricing</h1>
+
+      <Form layout="horizontal">
+        <Form.Item label="Rate Plan">
+          <RatePlanSelector
+            value={ratePlanParam}
+            onChange={handleRatePlanChange}
+          />
+        </Form.Item>
+        <Form.Item label="Wholesale Market">
+          <Select
+            options={Object.keys(HUB_DICT)
+              .toSorted()
+              .map((x) => {
+                return {
+                  label: x,
+                  value: x,
+                }
+              })}
+            onChange={(e) => {
+              updateState((state) => {
+                state.wholesale = e
+              })
+            }}
+            value={state.wholesale}
+          />
+        </Form.Item>
+        <Form.Item label="For Date">
+          <DatePicker
+            allowClear={false}
+            minDate={dayjs('2024-01-01')}
+            maxDate={dayjs('2024-12-31')}
+            value={state.date}
+            onChange={(e) =>
+              updateState((state) => {
+                state.date = e
+              })
+            }
+          />
+        </Form.Item>
+
+        <div>
+          {supersedesExistsInData ? (
+            <>
+              Supersedes{' '}
+              <Link to={`/detail/${selectedPlan?.supersedes}`}>
+                {selectedPlan?.supersedes}{' '}
+              </Link>
+            </>
+          ) : (
+            'Latest Plan'
+          )}
+        </div>
+      </Form>
+
+      <div id="my-div" ref={chartRef}></div>
+    </main>
+  )
+}
 
 // Function to prepare wholesale reference lines
 export function prepareWholesaleData(
@@ -190,98 +281,5 @@ function pullData(
         return result ?? []
       })
     }) ?? []
-  )
-}
-
-export default function DetailView() {
-  const { id: ratePlanParam } = useParams()
-  const { data: selectedPlan } = useRatePlan(ratePlanParam)
-
-  const { data: supersedesExistsInData } = useRatePlanInData(
-    selectedPlan?.supersedes
-  )
-  const [state, updateState] = useImmer<State>({
-    rawData: undefined,
-    adjustedIncluded: true,
-    date: dayjs().set('year', 2024),
-  })
-  const { data: wholesaleData } = useWholesaleData('New England', state.date)
-  const chartRef = useRef<HTMLDivElement>(null)
-  const retailData = pullData(selectedPlan, state.date.month())
-  const preparedWholesale = prepareWholesaleData(wholesaleData)
-  useVegaEmbed({
-    ref: chartRef,
-    spec: createPricingChartSpec(retailData, preparedWholesale),
-    options: { mode: 'vega-lite', actions: false },
-  })
-
-  useEffect(() => {
-    // Update raw view
-    updateState((state) => {
-      state.rawData = JSON.stringify(
-        selectedPlan ?? {},
-        (key, value) => {
-          if (typeof value === 'bigint') {
-            return value.toString()
-          }
-          return value
-        },
-        2
-      ).replaceAll(/\[\s+([^\[\]{}]+?)\s+\]/g, (match, content) => {
-        const cleaned = content.replace(/\s+/g, ' ').trim()
-        return `[${cleaned}]`
-      })
-    })
-  }, [selectedPlan])
-
-  const nav = useNavigate()
-
-  const handleRatePlanChange = async (value: string) => {
-    nav(`/detail/${value}`)
-  }
-
-  return (
-    <main className={s.main}>
-      <h1>Visualizing Dynamic Electricity Pricing</h1>
-
-      <Form layout="horizontal">
-        <Form.Item label="Rate Plan">
-          <RatePlanSelector
-            value={ratePlanParam}
-            onChange={handleRatePlanChange}
-          />
-        </Form.Item>
-        <Form.Item label="For Date">
-          <DatePicker
-            allowClear={false}
-            minDate={dayjs('2024-01-01')}
-            maxDate={dayjs('2024-12-31')}
-            value={state.date}
-            onChange={(e) =>
-              updateState((state) => {
-                state.date = e
-              })
-            }
-          />
-        </Form.Item>
-
-        <div>
-          {supersedesExistsInData ? (
-            <>
-              Supersedes{' '}
-              <Link to={`/detail/${selectedPlan?.supersedes}`}>
-                {selectedPlan?.supersedes}{' '}
-              </Link>
-            </>
-          ) : (
-            'Latest Plan'
-          )}
-        </div>
-      </Form>
-
-      <div id="my-div" ref={chartRef}></div>
-
-      <pre id="raw-view">{state.rawData}</pre>
-    </main>
   )
 }
