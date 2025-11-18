a9056af1051d100081d51361de03648c38258f28
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2025-11-17 20:57:44 -0500

    use modern router to fetch routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,19 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { lazy } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { lazy, StrictMode, Suspense } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom'
 import './global.css'
 import * as s from './App.module.css'
 
 const Storytelling = lazy(() => import('./pages/Storytelling'))
-const DetailView = lazy(() => import('./pages/DetailView'))
 const RegionalElectricityPatterns = lazy(() => import('./pages/ComparePlans'))
 
 const queryClient = new QueryClient({
@@ -17,30 +25,71 @@ const queryClient = new QueryClient({
   },
 })
 
+// Layout component for the nav
+function Layout() {
+  return (
+    <>
+      <nav className={s.nav}>
+        <Link className={s.title} to="/">
+          Visualizing Variable Electricity Pricing
+        </Link>
+        <div className={s.links}>
+          <Link to="/">Home</Link>
+          <Link to="/detail">Detail View</Link>
+          <Link to="/compare">Compare Plans</Link>
+        </div>
+      </nav>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        lazy: () =>
+          import('./pages/Storytelling').then((module) => ({
+            Component: module.default,
+          })),
+      },
+      {
+        path: 'detail',
+        lazy: () =>
+          import('./pages/DetailView').then((module) => ({
+            Component: module.default,
+          })),
+      },
+      {
+        path: 'detail/:id',
+        lazy: () =>
+          import('./pages/DetailView').then((module) => ({
+            Component: module.default,
+          })),
+      },
+      {
+        path: 'compare',
+        lazy: () =>
+          import('./pages/ComparePlans').then((module) => ({
+            Component: module.default,
+          })),
+      },
+    ],
+  },
+])
+
 // Trigger loading db on app load
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <nav className={s.nav}>
-          <Link className={s.title} to="/">
-            Visualizing Variable Electricity Pricing
-          </Link>
-          <div className={s.links}>
-            <Link to="/">Home</Link>
-            <Link to="/detail">Detail View</Link>
-            <Link to="/compare">Compare Plans</Link>
-          </div>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Storytelling />} />
-          <Route path="/detail" element={<DetailView />} />
-          <Route path="/detail/:id" element={<DetailView />} />
-          <Route path="compare" element={<RegionalElectricityPatterns />} />
-          {/* with params */}
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
   )
 }
