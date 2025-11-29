97081958d2e612c3130f71f2f8aea569cc1c3b0d
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2025-11-29 13:48:07 -0500

    add service territory and ba tables to duckdb

---
+++ data/service_territory.py
@@ -0,0 +1,42 @@
+"""Script to add balancingAuthority column to existing DuckDB database."""
+
+import polars as pl
+
+import duckdb
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+
+def load_ba_assignments() -> pl.DataFrame:
+    """Load BA assignments from EIA-861 Utility Data file."""
+    UTILITY_DATA = Path("raw") / "eia-861-2024" / "Service_Territory_2024.xlsx"
+
+    df = pl.read_excel(UTILITY_DATA)
+
+    print(df)
+
+    return df
+
+
+if __name__ == "__main__":
+    print("Adding balancingAuthority column to existing database...")
+
+    # Load BA assignments
+    df = load_ba_assignments()
+
+    # Connect to database
+    con = duckdb.connect("flattened.duckdb")
+
+    f = NamedTemporaryFile("w")
+    # Clean and write each document
+
+    df.write_json(f.name)
+
+    con.execute(
+        f"""
+    CREATE OR REPLACE TABLE eia861_service_territory AS
+    SELECT * FROM read_json('{f.name}')"""
+    )
+
+    con.close()
+    print("\n✅ Done!")
